3261fe1d8e90523d0ca233c514c4ab27252226a8
rmit-s3429922-timothy-snee <s3429922@student.rmit.edu.au>
2020-05-06 21:22:10 +1000

    Initial Set Up of SpringBoot

---
+++ src/main/java/rmitccassignment2/ccass2/Ccass2Application.java
@@ -0,0 +1,37 @@
+package rmitccassignment2.ccass2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Ccass2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ccass2Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
+}
