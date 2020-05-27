25259ec459a0dee39cb6dd8211c29d543abaa673
rmit-s3429922-timothy-snee <s3429922@student.rmit.edu.au>
2020-05-27 16:06:15 +1000

    changes

--- Backend/src/main/java/rmitccassignment2/ccass2/UserService.java
+++ Backend/src/main/java/rmitccassignment2/ccass2/UserService.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = { "http://localhost:3000" })
+@CrossOrigin(origins = { "*" })
 @RestController
 public class UserService {
 
